a8f1a8ed4a6d3372b28fc9a9d41ab308ebc6ac6f
Geoffrey Thomas <geofft@ldpreload.com>
2016-06-03 22:32:58 -0400

    Use os.execv in wrapper script instead of subprocess
    This fixes things like `patchelf --version`, since check_output
    was capturing the output, and also makes sure that `patchelf` with
    no arguments doesn't print a Python traceback.

--- pypatchelf/__init__.py
+++ pypatchelf/__init__.py
@@ -1,9 +1,8 @@
 import sys
-import os.path
-import subprocess
+import os
 
 PATCHELF = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'patchelf')
 
 
 def main():
-    subprocess.check_output([PATCHELF] + sys.argv[1:])
+    os.execv(PATCHELF, sys.argv)
